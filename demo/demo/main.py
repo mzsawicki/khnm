89bab408a00b8ab48325648b317ae9cc985f7e24
Marcin Sawicki <marcin.z.sawicki@protonmail.com>
2026-02-08 17:30:21 +0100

    Add simple demo

---
+++ demo/demo/main.py
@@ -0,0 +1,55 @@
+import asyncio
+import random
+import uuid
+from typing import AsyncGenerator, List
+
+import pydantic
+
+import khnm
+
+
+class Task(pydantic.BaseModel):
+    correlation_id: uuid.UUID = uuid.uuid4()
+    data: List[int] = pydantic.Field(default_factory=list)
+
+
+class SplitTask(pydantic.BaseModel):
+    correlation_id: uuid.UUID = uuid.uuid4()
+    data: int
+
+
+async def generate() -> AsyncGenerator[Task, None]:
+    for _ in range(100):
+        numbers = [random.randint(1, 10) for _ in range(10)]
+        yield Task(data=numbers)
+
+
+def split(task: Task) -> List[SplitTask]:
+    tasks = []
+    for number in task.data:
+        tasks.append(SplitTask(correlation_id=task.correlation_id, data=number))
+    return tasks
+
+
+async def sleep(task: SplitTask) -> SplitTask:
+    await asyncio.sleep(random.randint(1, 10))
+    return task
+
+
+def multiply(task: SplitTask) -> SplitTask:
+    return SplitTask(data=task.data * 100)
+
+
+def print_result(task: SplitTask) -> None:
+    print(task.data)
+
+
+pipeline = (
+    khnm.make_pipeline()
+    .add("generator", generate)
+    .add("splitter", split)
+    .add("sleeper", sleep)
+    .add("multiplier", multiply)
+    .add("printer", print_result)
+    .build()
+)
